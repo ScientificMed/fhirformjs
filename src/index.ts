b0315c64fe8ca1b7b44a583df8b9e86b15375499
dermatologist <github@gulfdoctor.net>
2020-05-21 15:48:10 -0400

    Add jest and index

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+
+
+ const ffjs = require('./fhirform');
+ const ffresp = require('./fhirform-resp');
+
+ module.exports = {
+  fhirformjs: ffjs,
+  fhirformResp: ffresp
+};
+
+
+
+//export const sum = (a: number, b: number): number => a + b
